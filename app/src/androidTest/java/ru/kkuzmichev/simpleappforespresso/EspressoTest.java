917c6ab358b6c01c3c40fb0e9dd08ac71ffa7046
Lyudmila Ostroumova <lyudmila.ostroumova@gmail.com>
2022-06-18 18:17:36 +0300

    Espresso task 2

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/EspressoTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/EspressoTest.java
@@ -1,17 +1,35 @@
 package ru.kkuzmichev.simpleappforespresso;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import android.content.Intent;
+
+import androidx.appcompat.widget.ActionMenuView;
+import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
 public class EspressoTest {
@@ -19,6 +37,17 @@ public class EspressoTest {
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule =
             new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void registerIdlingResources(){
+        IdlingRegistry.getInstance().register(EspressoIdlingResources.idlingResource);
+    }
+
+    @After
+    public void unregisterIdlingResources(){
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResources.idlingResource);
+    }
+
 
     @Test
     public void textTest() {
@@ -31,4 +60,36 @@ public class EspressoTest {
                 )
         );
     }
+
+    @Test
+    public void intentsCheck () {
+        ViewInteraction settings = onView(withParent(isAssignableFrom(ActionMenuView.class)));
+        ViewInteraction settingsItem = onView(allOf(withId(R.id.title), withText("Settings")));
+
+        settings.check(matches(isDisplayed()));
+        settings.perform(click());
+
+        settingsItem.check(matches(isDisplayed()));
+
+        Intents.init();
+        settingsItem.perform(click());
+        intended(allOf(
+                hasData("https://google.com"),
+                hasAction(Intent.ACTION_VIEW)
+        ));
+        Intents.release();
+    }
+
+    @Test
+    public void idlingWork() {
+        ViewInteraction menu = onView(isAssignableFrom(AppCompatImageButton.class));
+        menu.check(matches(isDisplayed()));
+        menu.perform(click());
+        ViewInteraction gallery = onView(withId(R.id.nav_gallery));
+        gallery.perform(click());
+        ViewInteraction element = (onView(allOf(withId(R.id.item_number), withText("7"))));
+        element.check(matches(isDisplayed()));
+    }
+
 }
+
